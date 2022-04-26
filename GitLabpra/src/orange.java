53306597c795cd42fdebb3c1194d1dc5294e011c
muska <muska@LAPTOP-DB02CP52>
2022-04-27 08:37:43 +1400

    The First Comiit for

---
+++ GitLabpra/src/orange.java
@@ -0,0 +1,22 @@
+
+public class orange {
+String color;
+	
+	orange(String color)
+	{
+		this.color=color;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		orange greenApple = new orange("green");
+		String color = greenApple.getColor();
+		System.out.println("color of apple is " + color);
+	}
+	private String getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+} 
+
+
+
